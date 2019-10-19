a1a1c00be36fc2aa1a1df1b7ba2285190fa257a2
dwu324 <dwu324@gatech.edu>
2019-10-19 16:53:32 -0500

    Update weight 2nd

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -17,7 +17,7 @@
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4'
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4',
 							  'http://loinc.org|29463-7']
                       }
                     }
@@ -38,6 +38,7 @@
           }
 
           var height = byCodes('8302-2');
+		  var weight = byCodes('29463-7');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
@@ -47,7 +48,7 @@
           p.birthdate = patient.birthDate;
           p.gender = gender;
           p.fname = fname;
-          p.lname = lname;
+          p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
 
           if (typeof systolicbp != 'undefined')  {
